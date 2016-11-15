9afb35395c45e65ed9ee171affca699c54f0b8f6
Marina Voinova <kolosiwka@gmail.com>
2016-11-15 15:14:33 +0100

    добавлен тест1
    Запускает браузер, открывает https://desktop.github.com страницу и
    закрывает браузер.

---
+++ Task1Setup/Test1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task1Setup
+{
+	using System;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using OpenQA.Selenium;
+	using OpenQA.Selenium.Support.UI;
+	using NUnit.Framework;
+	using OpenQA.Selenium.Chrome;
+
+	namespace csharp_example
+	{
+		[TestClass]
+		public class MyFirstTest
+		{
+			private IWebDriver driver;
+
+			[SetUp]
+			public void start()
+			{
+				driver = new ChromeDriver();
+			}
+
+			[Test]
+			public void FirstTest()
+			{
+				driver.Url = "https://desktop.github.com";
+			}
+
+			[TearDown]
+
+			public void stop()
+			{
+				driver.Quit();
+				driver = null;
+			}
+		}
+	}
+
+}
